aa88cd2569ce6e2f152fa221890e391f8f6272ae
Aldwin <13520167@std.stei.itb.ac.id>
2022-12-01 16:59:55 +0700

    feat: change error message to frontend

--- routes/auth/jwt.js
+++ routes/auth/jwt.js
@@ -3,15 +3,22 @@ const bcrypt = require("bcryptjs");
 module.exports = async (fastify, opts) => {
   fastify.post("/login", async (request, reply) => {
     const { username, password } = request.body;
+    
     const user = await fastify.prisma.user.findUnique({
       where: {
         username,
       },
     });
 
+    if(!user) {
+      reply.code(401).send({
+        msg: "wrong username or password",
+      })
+    };
+
     const match = await bcrypt.compare(password, user.password);
     if (!match) {
-      return reply.code(401).send({ error: "wrong password" });
+      return reply.code(401).send({ msg: "wrong username or password" });
     }
 
     const token = fastify.jwt.sign({
