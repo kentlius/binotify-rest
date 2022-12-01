cf85cf6733a3d7d4fb97c8e64790356860c2278d
Vionie Novencia Thanggestyo <13520006@std.stei.itb.ac.id>
2022-12-01 15:48:43 +0700

    feat: add get users filter

--- routes/users/getUsers.js
+++ routes/users/getUsers.js
@@ -1,9 +1,16 @@
 module.exports = async (fastify, opts) => {
   fastify.get(
     "/",
-    { onRequest: [fastify.authenticate], preHandler: [fastify.isadmin] },
     async (request, reply) => {
-      const users = await fastify.prisma.user.findMany();
+      const users = await fastify.prisma.user.findMany({
+        where:{
+          isadmin: false,
+        },
+        select: {
+          user_id: true,
+          name: true,
+        },
+      });
       return users;
     }
   );
